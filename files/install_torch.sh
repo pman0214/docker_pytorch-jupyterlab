c0ba967ac9e9761db48090b6254cb403caeadad0
Shigemi ISHIDA <ish@fun.ac.jp>
2025-11-06 07:42:32 +0900

    Updating to 2.9.0.

--- files/install_torch.sh
+++ files/install_torch.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Copyright (c) 2021-2024, Shigemi ISHIDA
+# Copyright (c) 2021-2025, Shigemi ISHIDA
 # 
 # Permission is hereby granted, free of charge, to any person obtaining
 # a copy of this software and associated documentation files (the
@@ -23,25 +23,10 @@
 
 cd $(dirname $0) || exit 1
 
-CUDA_TARGETS="torch==2.3.0+cu121
-torchvision==0.18.0+cu121
-torchaudio==2.3.0+cu121"
-TARGETS=""
-
-case $(arch) in
-    aarch64|arm64)
-        ;;
-    x86_64|amd64)
-        # build for cpu
-        lines=$(echo "${CUDA_TARGETS}" | cut -d'+' -f1 | while read line; do
-            echo $line+cpu
-        done)
-        CUDA_TARGETS=$lines
-        ;;
-    *)
-        exit 1
-esac
+TARGETS="torch==2.9.0
+torchvision==0.24.0
+torchaudio==2.9.0"
 
 set -x
 
-pip3 install ${CUDA_TARGETS} ${TARGETS} -f https://download.pytorch.org/whl/torch_stable.html || exit 1
+pip3 install ${TARGETS} --index-url https://download.pytorch.org/whl/cpu || exit 1
